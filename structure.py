242171fbb62f2943f5c955cb30dbe07c568c2eb7
South <nam@google.com>
2020-04-22 19:47:31 +0700

    add id foder, file

--- structure.py
+++ structure.py
@@ -11,17 +11,27 @@ def create_folder(path):
 		print('path exists')
 
 def create_file(path, file):
-	# try:
-	with open(os.path.join(path, file), 'w') as fp: 
-		pass
-	# except:
-	# 	print('create file error')
+	try:
+		with open(os.path.join(path, file), 'w') as fp: 
+			pass
+	except:
+		print('create file error')
+
+def create_file_id(path, file, _id):
+	try:
+		with open(os.path.join(path, file), 'w') as fp: 
+			fp.write(_id)
+	except:
+		print('create file error')
 
 def create_structure(root, drive):
 	name = drive['name']
+	_id = drive['id']
+
 	if drive['mimeType'] == 'application/vnd.google-apps.folder':
 		current_path = root + '/' + name.replace('/', '-')
 		create_folder(current_path)
+		create_file_id(current_path, '0id.txt', _id)
 
 		# if have children (because duplicate folder, some folder not get children)
 		if 'children' in drive:
@@ -29,7 +39,7 @@ def create_structure(root, drive):
 				create_structure(current_path, sub)
 
 	else:
-		create_file(root, name.replace('/', '-'))
+		create_file_id(root, name.replace('/', '-'), _id)
 
 with open('folder.txt', 'r') as f:
 	folder_id = f.read().strip()
